3c883ea5d2a3075185f790ef958b8c5c8470e863
angel-beltran100 <angelb3ltr4n2001@gmail.com>
2022-07-14 22:07:19 -0600

    lineas necesarias en el main

--- src/poo/ejercicio/pkg1/POOEjercicio1.java
+++ src/poo/ejercicio/pkg1/POOEjercicio1.java
@@ -14,7 +14,8 @@ public class POOEjercicio1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Persona persona1 = new Persona("34345432", "Juan", "Matias", "76589647", "juanmatias@gmail.com");
+        TarjetaDeCredito tarjeta1 = new TarjetaDeCredito(EntidadFinanciera.BIRZA, "Banco social", "0001", 15000, persona1);
     }
     
 }
